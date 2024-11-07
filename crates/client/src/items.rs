1a738d8ef4486e99e25028386681ea1f4d4f1995
kotfind <kotfind@yandex.ru>
2024-11-07 21:34:12 +0300

    client::Connection::get_item

--- crates/client/src/items.rs
+++ crates/client/src/items.rs
@@ -15,7 +15,59 @@ impl Connection {
     }
 }
 
+impl Connection {
+    pub async fn get_item(&self, name: impl Into<Ident>) -> anyhow::Result<Value> {
+        let resp = self
+            .req(api::items::get::Request { name: name.into() })
+            .await?;
+
+        Ok(resp.value)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    // tested in exec.rs
+    use super::*;
+
+    #[tokio::test]
+    async fn get_all() {
+        let con = Connection::new_test().await.unwrap();
+
+        con.exec(
+            r#"
+            x = 1
+            y = 2
+            z = x + y
+        "#,
+        )
+        .await
+        .unwrap();
+
+        let items = con.get_all_items().await.unwrap();
+
+        assert!(items.len() == 3);
+        assert!(items[&Ident::from("x")] == 1.into());
+        assert!(items[&Ident::from("y")] == 2.into());
+        assert!(items[&Ident::from("z")] == 3.into());
+    }
+
+    #[tokio::test]
+    async fn get_item() {
+        let con = Connection::new_test().await.unwrap();
+
+        con.exec(
+            r#"
+            x = 1
+            y = 2
+            z = x + y
+        "#,
+        )
+        .await
+        .unwrap();
+
+        assert!(con.get_item("x").await.unwrap() == 1.into());
+        assert!(con.get_item("y").await.unwrap() == 2.into());
+        assert!(con.get_item("z").await.unwrap() == 3.into());
+        assert!(con.get_item("t").await.is_err());
+    }
 }
