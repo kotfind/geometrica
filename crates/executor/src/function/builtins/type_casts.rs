226510cc1a2902de2d74a6fcce948d783514d301
kotfind <kotfind@yandex.ru>
2024-11-09 13:53:41 +0300

    Fix formatting in function/builtins/type_casts.rs

--- crates/executor/src/function/builtins/type_casts.rs
+++ crates/executor/src/function/builtins/type_casts.rs
@@ -4,102 +4,102 @@ pub(super) fn populate(builtins: &mut FuncMap) {
     simple_builtin!(INTO builtins INSERT
         // As
         fn "#as_bool" (v: Bool) -> Bool { v }
-        fn "#as_bool" (v: Int) -> Bool { v != 0 }
+        fn "#as_bool" (v:  Int) -> Bool { v != 0 }
         fn "#as_bool" (v: Real) -> Bool { v != 0.0 }
-        fn "#as_int" (v: Bool) -> Int { if v { 1 } else { 0 } }
-        fn "#as_int" (v: Int) -> Int { v }
-        fn "#as_int" (v: Real) -> Int { v as i64 }
+        fn "#as_int"  (v: Bool) -> Int  { if v { 1 } else { 0 } }
+        fn "#as_int"  (v:  Int) -> Int  { v }
+        fn "#as_int"  (v: Real) -> Int  { v as i64 }
         fn "#as_real" (v: Bool) -> Real { if v { 1.0 } else { 0.0 } }
-        fn "#as_real" (v: Int) -> Real { v as f64 }
+        fn "#as_real" (v:  Int) -> Real { v as f64 }
         fn "#as_real" (v: Real) -> Real { v }
     );
 
     // TODO?: cast str -> *
     // TODO: optimize as_str (don't construct value after just deconstruncting it)
     builtin!(INTO builtins INSERT
-        fn "#as_str" (v: Bool) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Int) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Real) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Str) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:  Bool) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:   Int) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:  Real) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:   Str) -> Str { Ok(Value::from(v).to_string()) }
         fn "#as_str" (v: Array) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Pt) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Line) -> Str { Ok(Value::from(v).to_string()) }
-        fn "#as_str" (v: Circ) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:    Pt) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:  Line) -> Str { Ok(Value::from(v).to_string()) }
+        fn "#as_str" (v:  Circ) -> Str { Ok(Value::from(v).to_string()) }
         // Is
-        fn "#is_bool" (_v: Bool) -> Bool { Ok(true) }
-        fn "#is_bool" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_bool" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Int) -> Bool { Ok(true) }
-        fn "#is_int" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_int" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Real) -> Bool { Ok(true) }
-        fn "#is_real" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_real" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Str) -> Bool { Ok(true) }
-        fn "#is_str" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_str" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Str) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:  Bool) -> Bool { Ok(true) }
+        fn "#is_bool"  (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v: Array) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_bool"  (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:   Int) -> Bool { Ok(true) }
+        fn "#is_int"   (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_int"   (_v: Array) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_int"   (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:  Real) -> Bool { Ok(true) }
+        fn "#is_real"  (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_real"  (_v: Array) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_real"  (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:   Str) -> Bool { Ok(true) }
+        fn "#is_str"   (_v: Array) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_str"   (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_array" (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_array" (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_array" (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_array" (_v:   Str) -> Bool { Ok(false) }
         fn "#is_array" (_v: Array) -> Bool { Ok(true) }
-        fn "#is_array" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_array" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Pt) -> Bool { Ok(true) }
-        fn "#is_pt" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_pt" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_line" (_v: Line) -> Bool { Ok(true) }
-        fn "#is_line" (_v: Circ) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Bool) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Int) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Real) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Str) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Array) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Pt) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Line) -> Bool { Ok(false) }
-        fn "#is_circ" (_v: Circ) -> Bool { Ok(true) }
+        fn "#is_array" (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_array" (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_array" (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v: Array) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:    Pt) -> Bool { Ok(true) }
+        fn "#is_pt"    (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_pt"    (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_line"  (_v: Array) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_line"  (_v:  Line) -> Bool { Ok(true) }
+        fn "#is_line"  (_v:  Circ) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:  Bool) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:   Int) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:  Real) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:   Str) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v: Array) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:    Pt) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:  Line) -> Bool { Ok(false) }
+        fn "#is_circ"  (_v:  Circ) -> Bool { Ok(true) }
         // TODO optimize is_ construction
         // is_none
-        fn "#is_none" (v: Bool) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Int) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Real) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Str) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:  Bool) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:   Int) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:  Real) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:   Str) -> Bool { Ok(v.is_none()) }
         fn "#is_none" (v: Array) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Pt) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Line) -> Bool { Ok(v.is_none()) }
-        fn "#is_none" (v: Circ) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:    Pt) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:  Line) -> Bool { Ok(v.is_none()) }
+        fn "#is_none" (v:  Circ) -> Bool { Ok(v.is_none()) }
     );
 }
 
