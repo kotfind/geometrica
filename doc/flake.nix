ab3105470e289111a3b94eda4879b0ff09b76a29
kotfind <kotfind@yandex.ru>
2025-04-06 17:12:21 +0300

    Install corefonts (for Times New Roman)

--- doc/flake.nix
+++ doc/flake.nix
@@ -1,15 +1,36 @@
 {
-    outputs = { nixpkgs, ... }:
-        let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs { inherit system; };
-        in
-        {
-            devShells.${system}.default = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                    typst
-                    zathura
-                ];
-            };
-        };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        # for corefonts
+        config.allowUnfree = true;
+      };
+
+      inherit (pkgs) mkShell;
+
+      shell = mkShell {
+        name = "geometrica-doc-shell";
+
+        buildInputs = with pkgs; [
+          typst
+          zathura
+          corefonts
+        ];
+
+        TYPST_FONT_PATHS = "${pkgs.corefonts}/share/fonts/truetype/";
+      };
+    in {
+      devShell = shell;
+    });
 }
