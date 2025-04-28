01308d4f515ab8de939d2f36482ab39413b44023
kotfind <kotfind@yandex.ru>
2025-04-28 03:50:49 +0300

    gui: add more connection options

--- crates/gui/src/main_w/disconnected_w.rs
+++ crates/gui/src/main_w/disconnected_w.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 use client::{Client, ClientSettings};
 use iced::{
     font::Weight,
-    widget::{button, column, container, text, text_input},
+    widget::{button, checkbox, column, container, text, text_input},
     Alignment::Center,
     Element, Font,
     Length::Fill,
@@ -14,6 +14,7 @@ use crate::{helpers::perform_or_status, status_bar_w::StatusMessage};
 #[derive(Debug)]
 pub struct State {
     server_url_input: String,
+    try_spawn_server: bool,
 }
 
 impl Default for State {
@@ -22,16 +23,19 @@ impl Default for State {
             server_url_input: ClientSettings::DEFAULT_URL
                 .parse()
                 .expect("DEFAULT_URL can be parsed to url"),
+            try_spawn_server: true,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum Msg {
-    Connected(Client),
-    ServerUrlInputChanged(String),
-    Connect,
     SetStatusMessage(StatusMessage),
+    Connected(Client),
+    Connect,
+
+    ServerUrlInputChanged(String),
+    SetTrySpawnServer(bool),
 }
 
 impl State {
@@ -40,46 +44,56 @@ impl State {
             weight: Weight::Bold,
             ..Default::default()
         });
-        let input =
+
+        let server_url_input =
             text_input("Server Url", &self.server_url_input).on_input(Msg::ServerUrlInputChanged);
 
-        let btn = button("Connect").on_press(Msg::Connect);
+        let try_spawn_server_check =
+            checkbox("Try Spawn Server", self.try_spawn_server).on_toggle(Msg::SetTrySpawnServer);
 
-        let content = column![title, input, btn].spacing(5).align_x(Center);
+        let submit_btn = button("Connect").on_press(Msg::Connect);
 
-        let inner = container(content).max_width(300);
+        let ans = column![title, server_url_input, try_spawn_server_check, submit_btn]
+            .spacing(10)
+            .align_x(Center);
 
-        container(inner)
+        let ans = container(ans).max_width(300);
+
+        let ans = container(ans)
             .width(Fill)
             .height(Fill)
             .align_x(Center)
-            .align_y(Center)
-            .into()
+            .align_y(Center);
+
+        ans.into()
     }
 
     pub fn update(&mut self, msg: Msg) -> Task<Msg> {
         match msg {
-            Msg::Connected(_) => {
-                unreachable!("This message should have been processed in a parent widget, and not forwarded here");
+            Msg::Connected(_) | Msg::SetStatusMessage(_) => {
+                unreachable!("should have been processed in a parent widget");
             }
             Msg::ServerUrlInputChanged(url) => {
                 self.server_url_input = url;
                 Task::none()
             }
             Msg::Connect => {
-                perform_or_status!(Self::connect(self.server_url_input.clone()), Msg::Connected)
+                perform_or_status!(
+                    Self::connect(self.server_url_input.clone(), self.try_spawn_server),
+                    Msg::Connected
+                )
             }
-            Msg::SetStatusMessage(_) => {
-                unreachable!("this message should have been processed in a parent widget");
+            Msg::SetTrySpawnServer(try_spawn_server) => {
+                self.try_spawn_server = try_spawn_server;
+                Task::none()
             }
         }
     }
 
-    async fn connect(server_url: String) -> anyhow::Result<Client> {
-        // TODO: more settings
+    async fn connect(server_url: String, try_spawn_server: bool) -> anyhow::Result<Client> {
         let client = Client::from(ClientSettings {
             server_url: server_url.parse().context("failed to parse server url")?,
-            ..Default::default()
+            try_spawn_server,
         })
         .await
         .context("failed to connect")?;
